5098db6755f53859e81ca2b970e86c0a8dc9c63d
Billy Chiu <594billy@gmail.com>
2021-07-28 23:46:04 +0800

    show the times

--- guess_num.py
+++ guess_num.py
@@ -1,12 +1,17 @@
 import random
 x = random.randint(0, 100)
+count = 0
 while True:
     y = input('請猜數字:')
+    count = count + 1
     y = int(y)
     if y == x:
         print('終於猜對了！')
+        print('你總共猜了', count, '次！')
         break
     elif y > x:
         print('比答案大')
+
     else:
         print('比答案小')
+    print('你總共猜了', count, '次！')
